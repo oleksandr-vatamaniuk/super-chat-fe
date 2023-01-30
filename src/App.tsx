6cf158f26f426347168c6b8c4c7098f38d4901da
Vatamaniuk <oleksandr.vatamaniuk@sombrainc.com>
2023-01-30 16:20:53 +0200

    configure prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import './App.scss';
+import React from 'react'
+import './App.scss'
 
 function App() {
-	return (
-		<div className="App">
-			<h1>Hello World!</h1>
-			<ul>
-				<li>Item 1</li>
-				<li>Item 2</li>
-			</ul>
-		</div>
-	);
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+      </ul>
+    </div>
+  )
 }
 
-export default App;
+export default App
